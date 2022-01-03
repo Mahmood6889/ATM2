81eaf69939060f74ff66d0e2db7fee3de408a4ef
mahmood6889 <mahmood_sh6889@yahoo.com>
2022-01-03 21:20:14 +0330

    add

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout<<"ramzo vared kon";
+int z =1234;
+int p;
+cin>>p;
+if(z==p)
+{
+
+
+  
+  cout<<"goziney morede nazar ra entekhab konid"<<endl<<"1_mojodi"<<endl<<"2_5 gardesh akhar"<<endl<<"3_bardasht az hesab"<<endl<<
+  "4_taghir ramz"<<endl;}
+  
+  else {
+  	cout<<"ramz nadorost ast";
+  }
+  
+  int x;
+  
+  cin>>x;
+  
+  switch (x) {
+  case 1:
+    cout << "Mojdiye shoma = 12000"<<endl;
+    break;
+  case 2:
+    cout << "5 gardesh akhar"<<endl<<"1.+5000"<<endl<<"2.-7000"<<endl<<"3.+8000"<<endl<<"4.-6000"<<"\n"<<"5.+1000"<<endl;
+    break;
+  case 3:
+    cout << "mablagh ra vared konid"<<endl;
+     int k;
+	 cin>>k;
+	 if(k>12000) 
+	 {   
+	     cout<<"mojodi kafi nemibashad"<<endl;
+	    
+	 }else 
+	 {
+	 	 cout<<"lotfan pol khod ra bardarid"<<endl;
+	 }
+    break;
+  case 4:
+    cout << "ramz jadid ra vared konid"<<endl;
+     int r;
+     cin>>r;
+
+     
+    break;
+    
+  }
+  cout<<"aya darkhast dg dari"<<endl<<"1_bale"<<"\n"<<"2_na"<<endl;
+   int y;
+    cin>>y;
+    if(y==1)
+    {
+    	cout<< main();
+	}else
+	{ cout<<"roz khobi dashte bashin";
+	}
+   	
+  
+  
+  return 0;
+}
+
